1a04a4ed681d901711de61ff2e93d5ed49986c07
3a917032 <jk478478478@gmail.com>
2025-07-01 11:00:05 +0800

    新增爬取內文函式

--- crawler.py
+++ crawler.py
@@ -8,10 +8,15 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 start_time = time.time()
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+}
+
 # PTT 股票版爬蟲
 # 目標：抓取最新5頁中包含「川普」的文章，並寫入資料庫
 
 session = requests.Session() # 重複利用連線
+session.headers.update(HEADERS) # 設定 User-Agent
 
 with open("log.txt", "a", encoding="utf-8") as f:
     f.write(f"爬蟲執行時間：{datetime.now()}\n")
@@ -48,10 +53,46 @@ def process_page(page_num):
                     link = "https://www.ptt.cc" + title_tag["href"]
                     author = entry.select_one(".author").text.strip()
                     date = entry.select_one(".date").text.strip()
-                    if insert_news(date, title, link, ""):
+                    content = get_article_content(link)
+                    if insert_news(date, title, link,content):
                         send_to_discord(title, link)
     except Exception as e:
         print(f"第 {page_num} 頁失敗：{e}")
+
+# 擷取文章內文
+def get_article_content(url):
+    try:
+        resp = session.get(url)
+        if resp.status_code != 200:
+            return "內文載入失敗"
+        soup = BeautifulSoup(resp.text, "lxml")
+        main = soup.select_one("#main-content")
+        if not main:
+            return "找不到主內容"
+        content_lines = []
+        metaline_count = 0
+        start_collecting = False
+        for elem in main.children:
+            # 遇到 article-metaline，就累加
+            if getattr(elem, "name", None) == "div" and "article-metaline" in elem.get("class", []):
+                metaline_count += 1
+                continue
+            # 從第3個 article-metaline 之後開始收集,因為前3個是標題欄
+            if metaline_count >= 3 and not start_collecting:
+                start_collecting = True
+                continue  # 略過當下這一個（通常是分隔或空行）
+            # 遇到 span.f2 就結束,這是發信人ip
+            if getattr(elem, "name", None) == "span" and "f2" in elem.get("class", []):
+                break
+            if start_collecting:
+                if isinstance(elem, str):
+                    content_lines.append(elem.strip())
+                else:
+                    content_lines.append(elem.get_text(strip=True))
+
+        return "\n".join([line for line in content_lines if line])
+    except Exception as e:
+        return f"取得內文錯誤：{e}"
 
 # === 主程式 ===
 latest = get_latest_page()
